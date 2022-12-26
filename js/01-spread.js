41938b70f85f29eee5fc913ba2e29b02b5f84eb5
UA-Viktor <v.korenkov@icloud.com>
2022-12-26 11:11:31 +0200

    test

---
+++ js/01-spread.js
@@ -0,0 +1,22 @@
+// Операция spread распыление
+// Array.prototype.concat() и аналог через spread
+
+// Поиск самой маленькой или большой температуры ( числа )
+
+const temps = [18, 14, 12, 21, 17, 29, 25];
+
+
+
+
+
+// Считывает несколько массмвов в один через concat() и spread
+
+
+const lastWeekTemps = [1, 2, 3];
+const currentTemps = [4, 5, 6];
+const nextWeekTemps = [7, 8, 9];
+
+const allTemps = [];
+
+
+// Распыление объектов
